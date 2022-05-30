42838af542fc4081e072fa5c7412a63c5807c520
mwuyan <mwuyan@yahoo.com>
2022-05-29 20:23:11 -0500

    methods

---
+++ parse_methods.py
@@ -0,0 +1,36 @@
+import nltk
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+import spacy
+
+
+primary = ['fry', 'saute', 'bake', 'roast', 'boil', 'steam', 'broil', 'poach', 'grill']
+
+nlp = spacy.load("en_core_web_sm")
+
+class Method:
+    def __init__(self, input, order):
+        self.order = order
+        self.primary_cooking = []
+        self.secondary_cooking = []
+        self.direction = input
+
+
+def parse_method(direction, i ):
+    """ tokens = pos_tag(word_tokenize(directions[i].lower()))
+    print(tokens)
+    for j in range(len(tokens)):
+        if tokens[j][1] == 'VB' or tokens[j][1] == 'VBP':
+            print(tokens[j], i) """
+    curr_method = Method(direction, i)
+    doc = nlp(direction.lower())
+    for token in doc:
+        #print(token.text, token.pos_)
+        if token.pos_ == 'VERB':
+            if token.text in primary and not curr_method.primary_cooking:
+                curr_method.primary_cooking.append(token.lemma_)
+            else:
+                curr_method.secondary_cooking.append(token.lemma_)
+    if curr_method.secondary_cooking and not curr_method.primary_cooking:
+        curr_method.primary_cooking.append(curr_method.secondary_cooking.pop())
+    return curr_method
